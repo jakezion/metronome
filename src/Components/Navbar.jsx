1a2288463288b48eaad59428fb25ba2a7c266a74
jakezion <jakesumner@live.co.uk>
2022-08-24 00:59:28 +0100

    added navbar with settings for changing metronome sound

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {IoSettingsOutline, IoSettingsSharp, IoMusicalNoteSharp, IoMicSharp} from "react-icons/io5";
+import {FaDrum, FaGamepad, FaPlug} from "react-icons/fa";
+
+export default class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+                <nav className="settings">
+                    <ul className="settings-nav">
+                        <Dropdown>
+                            <DropdownItem icon={<FaGamepad className="icons"/>} title="8-Bit"/>
+                            <DropdownItem icon={<FaDrum/>} title="Drum"/>
+                            <DropdownItem icon={<FaPlug/>} title="Beep"/>
+                        </Dropdown>
+                    </ul>
+                </nav>
+            </>
+        )
+    }
+
+}
+
+function DropdownItem(props) {
+    return (
+        <div className="dropdown-item">
+            <button className="dropdown-item-button">{props.icon}&nbsp;&nbsp;{props.title}</button>
+        </div>
+    );
+}
+
+function Dropdown(props) {
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <div>
+                <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+                    {open === false ? <IoSettingsSharp className="settings-button"/> :
+                        <IoSettingsOutline className="settings-button"/>}
+                </a>
+                {open ? <div className="dropdown">Select Sound{props.children}   </div> : <></>}
+            </div>
+        </>
+    );
+}
+
