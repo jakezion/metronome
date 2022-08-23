465120e0162920d650e578c48ff5fc338beb0df4
jakezion <jakesumner@live.co.uk>
2022-08-24 00:57:26 +0100

    stylisation changes

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,22 @@
+import {IoLogoGithub} from "react-icons/io5";
+import React from "react";
+
+export default class Footer extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <footer>
+                <a href="https://github.com/jakezion/metronome/" target="_blank" rel="noreferrer">
+                    <div className="footer-details">
+                   <IoLogoGithub className="icons"/>&nbsp;Github repository&nbsp;</div></a>
+
+            </footer>
+        );
+    }
+
+
+}
