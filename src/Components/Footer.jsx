b255a980d3e2a3b35febe2bde942c476a3b229e7
jakezion <jakesumner@live.co.uk>
2022-08-26 09:35:36 +0100

    added commenting

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,7 @@
 import {IoLogoGithub} from "react-icons/io5";
 import React from "react";
 
+//basic footer with link to github repository
 export default class Footer extends React.Component {
 
     constructor(props) {
